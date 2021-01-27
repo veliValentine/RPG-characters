3810e698958115cc4ff5c3ae0aa43c7bbf0fa224
veliValentine <nikkevanukas1995@gmail.com>
2021-01-27 16:26:35 +0200

    feat: added Ranger hero

--- src/Hero/Ranger.java
+++ src/Hero/Ranger.java
@@ -1,2 +1,18 @@
-package Hero;public class Ranger {
+package Hero;
+
+public class Ranger extends Hero {
+
+    public Ranger() {
+        super(120, 5, 10, 2);
+    }
+
+    public void addExperience(int xp) {
+        addExperienceToHero(xp, 20, 2, 5, 1);
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Ranger details: ");
+        printStats();
+    }
 }
